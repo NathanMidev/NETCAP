755a34c4a17f0842ee9007d974f14b9134d8beed
NathanMidev <nambinintsoarandriantiana@gmail.com>
2026-03-31 22:23:28 +0300

    Sprint 2 part#:# creation de socket configuration du serveur sans gestion par thread

---
+++ proxy/proxy_server.py
@@ -0,0 +1,55 @@
+import socket, sys
+import threading
+
+#définition d'un serveur réseau rudimentaire
+# ce serveur attzend la connexion d'un client
+
+# hostname = socket.gethostname()
+#la configuration du serveur
+HOST = "localhost"
+PORT = 8080
+counter = 0 #comteur des host connectés
+
+## 1) creation du socket
+monsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+## 2) liaison du socket à une adresse et un port
+try:
+    monsocket.bind((HOST, PORT))#liaison
+except socket.error as e :
+    print(f"La liaison  de socket à 'adresse {HOST} et port {PORT} a échoué: {e}")
+    sys.exit() #arret du programme en cas d'erreur
+
+while 1:
+    ## 3) attent du socket à une connexion d'un client
+    print("Servuer prêt, en attente de connexion...")
+    monsocket.listen(5) #le serveur peut gérer jusqu'à 5 connexions simultanément
+
+    ## 4) etablissement de la connexion 
+    connexion, adresse = monsocket.accept()
+    counter += 1
+    print("Client connecté depuis l'adresse: ", adresse, "Nombre de clients connectés: ", counter)
+
+    ## 5) Dialogue avec le client
+    messageServeur ="Bienvenue , vous vous êtes connecté avec succès"
+    connexion.send(messageServeur.encode('utf-8')) #envoi du message de bienvenue au client
+    messsageClient = connexion.recv(1024).decode('utf-8') #réception du message du client
+    while True:
+        print("Client: ", messsageClient)
+        if messsageClient.upper() == "BYE" or messsageClient == "":
+            print("Client déconnecté: ", adresse)
+            counter -= 1
+            break##FIN DE LA CONNEXION ET NOMBRE DE CLIENTS CONNECTÉS DIMINUÉ DE 1
+        messageServeur = input("Serveur: ") #saisie du message à envoyer au client
+        connexion.send(messageServeur.encode('utf-8')) #envoi du message au client
+        messsageClient = connexion.recv(1024).decode('utf-8') #réception du message du client
+
+    ## 6) fermeture de la connexion
+    connexion.send("BYE".encode('utf-8')) #envoi du message de fin de connexion au client
+    connexion.close() #fermeture de la connexion
+
+    ch = input("Voulez-vous continuer? (o/n): ")#applictaion de regex pour vérifier la saisie de l'utilisateur
+    if ch.lower() != 'o':
+        print("Fermeture du serveur...")
+        break
+    #commande pour lancer le serveur : python main.py
